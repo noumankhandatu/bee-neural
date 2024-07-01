9617409d51f27e8492859239a961a115fa717e34
noumandatu <noumankhan619.915@gmail.com>
2024-07-01 18:46:36 +0500

    asd

--- app/components/molecules/hello-parallax/hero-parallax.tsx
+++ app/components/molecules/hello-parallax/hero-parallax.tsx
@@ -57,6 +57,7 @@ export const HeroParallax = ({
   );
   return (
     <div
+      id="home"
       ref={ref}
       className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d] bg-secondary"
     >
