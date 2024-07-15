1e651980f1ccdcc87a4e290833827bf17d68fd16
noumandatu <noumankhan619.915@gmail.com>
2024-07-15 16:43:38 +0500

    tool

--- app/(pages)/career/page.tsx
+++ app/(pages)/career/page.tsx
@@ -11,6 +11,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { IoMdArrowBack } from "react-icons/io";
 import Title from "@/app/components/atoms/Title";
+import Wrapper from "@/app/components/molecules/headerfooter-wrapper";
 
 const CareerPage = () => {
   const [formData, setFormData] = useState({
@@ -71,120 +72,122 @@ const CareerPage = () => {
   };
 
   return (
-    <div id="contact" className="container mx-auto py-10">
-      <div className="flex justify-between w-full">
-        <Link href={"/"}>
-          <IoMdArrowBack fontSize={35} color="#EE9D1A" className="ml-4" />
-        </Link>
+    <Wrapper>
+      <div id="contact" className="container mx-auto py-10">
+        <div className="flex justify-between w-full">
+          <Link href={"/"}>
+            <IoMdArrowBack fontSize={35} color="#EE9D1A" className="ml-4" />
+          </Link>
 
-        <p className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl text-black">
-          <Title />
-          👩🏻‍💻
+          <p className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl text-black">
+            <Title />
+            👩🏻‍💻
+          </p>
+          <div></div>
+        </div>
+
+        <p className="text-[20px] text-center mt-4 mb-12">
+          Join BeeNeural and Be a part of something great
         </p>
-        <div></div>
-      </div>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 w-[80%]">
+            <div className="w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+              <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+                Fill form below 🙌
+              </h2>
+              <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                Remember to drop your resume below
+              </p>
 
-      <p className="text-[20px] text-center mt-4 mb-12">
-        Join BeeNeural and Be a part of something great
-      </p>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 w-[80%]">
-          <div className="w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-            <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-              Fill form below 🙌
-            </h2>
-            <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-              Remember to drop your resume below
-            </p>
-
-            <form className="my-8" onSubmit={handleSubmit}>
-              <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                <LabelInputContainer>
-                  <Label htmlFor="firstName">First name</Label>
+              <form className="my-8" onSubmit={handleSubmit}>
+                <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+                  <LabelInputContainer>
+                    <Label htmlFor="firstName">First name</Label>
+                    <Input
+                      id="firstName"
+                      name="firstName"
+                      placeholder="Tyler"
+                      type="text"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      disabled={loading}
+                    />
+                  </LabelInputContainer>
+                  <LabelInputContainer>
+                    <Label htmlFor="lastName">Last name</Label>
+                    <Input
+                      id="lastName"
+                      name="lastName"
+                      placeholder="Durden"
+                      type="text"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      disabled={loading}
+                    />
+                  </LabelInputContainer>
+                </div>
+                <LabelInputContainer className="mb-4">
+                  <Label htmlFor="email">Email Address</Label>
                   <Input
-                    id="firstName"
-                    name="firstName"
-                    placeholder="Tyler"
-                    type="text"
-                    value={formData.firstName}
+                    id="email"
+                    name="email"
+                    placeholder="projectmayhem@fc.com"
+                    type="email"
+                    value={formData.email}
                     onChange={handleChange}
                     disabled={loading}
                   />
                 </LabelInputContainer>
-                <LabelInputContainer>
-                  <Label htmlFor="lastName">Last name</Label>
+                <LabelInputContainer className="mb-4">
+                  <Label htmlFor="message">Message</Label>
                   <Input
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Durden"
+                    id="message"
+                    name="message"
+                    placeholder="Write your humble words 🙂"
                     type="text"
-                    value={formData.lastName}
+                    value={formData.message}
                     onChange={handleChange}
                     disabled={loading}
                   />
                 </LabelInputContainer>
-              </div>
-              <LabelInputContainer className="mb-4">
-                <Label htmlFor="email">Email Address</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  placeholder="projectmayhem@fc.com"
-                  type="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  disabled={loading}
-                />
-              </LabelInputContainer>
-              <LabelInputContainer className="mb-4">
-                <Label htmlFor="message">Message</Label>
-                <Input
-                  id="message"
-                  name="message"
-                  placeholder="Write your humble words 🙂"
-                  type="text"
-                  value={formData.message}
-                  onChange={handleChange}
-                  disabled={loading}
-                />
-              </LabelInputContainer>
-              <div className="bg-gradient-to-r from-transparent via-neutral-300  to-transparent my-8 h-[1px] w-full" />
+                <div className="bg-gradient-to-r from-transparent via-neutral-300  to-transparent my-8 h-[1px] w-full" />
 
-              <LabelInputContainer className="mb-4">
-                <Label htmlFor="resume">Upload Resume (PDF)</Label>
-                <input
-                  id="resume"
-                  name="resume"
-                  type="file"
-                  accept="application/pdf"
-                  onChange={handleFileChange}
+                <LabelInputContainer className="mb-4">
+                  <Label htmlFor="resume">Upload Resume (PDF)</Label>
+                  <input
+                    id="resume"
+                    name="resume"
+                    type="file"
+                    accept="application/pdf"
+                    onChange={handleFileChange}
+                    disabled={loading}
+                    className="block w-full text-sm text-primary file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-gradient-to-br file:from-primary file:to-primary file:text-white hover:file:bg-gray-100 "
+                  />
+                </LabelInputContainer>
+
+                <button
+                  className="bg-gradient-to-br relative group/btn from-primary  to-primary block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                  type="submit"
                   disabled={loading}
-                  className="block w-full text-sm text-primary file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-gradient-to-br file:from-primary file:to-primary file:text-white hover:file:bg-gray-100 "
-                />
-              </LabelInputContainer>
+                >
+                  {loading ? "Sending..." : "Submit →"}
+                  <BottomGradient />
+                </button>
 
-              <button
-                className="bg-gradient-to-br relative group/btn from-primary  to-primary block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-                type="submit"
-                disabled={loading}
-              >
-                {loading ? "Sending..." : "Submit →"}
-                <BottomGradient />
-              </button>
-
-              <div className="bg-gradient-to-r from-transparent via-neutral-300  to-transparent my-8 h-[1px] w-full" />
-            </form>
+                <div className="bg-gradient-to-r from-transparent via-neutral-300  to-transparent my-8 h-[1px] w-full" />
+              </form>
+            </div>
+            <Image
+              src={"/assets/images/beeneural.webp"}
+              alt="image"
+              height={1000}
+              width={1000}
+              className="w-full h-full"
+            />
           </div>
-          <Image
-            src={"/assets/images/beeneural.webp"}
-            alt="image"
-            height={1000}
-            width={1000}
-            className="w-full h-full"
-          />
         </div>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
